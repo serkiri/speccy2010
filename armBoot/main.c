4a07ca2aa556b3d0c466851388cac4820992bdc9
Sergiy Kyrylenko <sergiy.kyrylenko@travisperkins.co.uk>
2017-01-01 23:57:56 +0200

    Scramble flash test

--- armBoot/main.c
+++ armBoot/main.c
@@ -6,7 +6,7 @@
 
 #include "system.h"
 
-#define MAIN_PROG_START 0x20008000
+#define MAIN_PROG_START 0x20002000
 
 bool MRCC_Config(void)
 {
@@ -37,41 +37,6 @@ bool MRCC_Config(void)
 	MRCC_PeripheralClockConfig( MRCC_Peripheral_SSP0, ENABLE );
 
 	return i > 0;
-}
-
-void SPI_Config()
-{
-    GPIO_InitTypeDef  GPIO_InitStructure;
-
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_Init(GPIO0, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_Init( GPIO1, &GPIO_InitStructure );
-
-	GPIO_WriteBit( GPIO1, GPIO_Pin_10, Bit_SET );
-	GPIO_WriteBit( GPIO0, GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7, Bit_SET );
-
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_18 | GPIO_Pin_19;
-	GPIO_Init( GPIO2, &GPIO_InitStructure );
-
-	//----------------------------------------------------------------------------------------------------
-
-    SSP_InitTypeDef   SSP_InitStructure;
-    SSP_InitStructure.SSP_FrameFormat = SSP_FrameFormat_Motorola;
-    SSP_InitStructure.SSP_Mode = SSP_Mode_Master;
-    SSP_InitStructure.SSP_CPOL = SSP_CPOL_Low;
-    SSP_InitStructure.SSP_CPHA = SSP_CPHA_1Edge;
-    SSP_InitStructure.SSP_DataSize = SSP_DataSize_8b;
-    SSP_InitStructure.SSP_NSS = SSP_NSS_Soft;
-    SSP_InitStructure.SSP_ClockRate = 0;
-    SSP_InitStructure.SSP_ClockPrescaler = 2; /* SSP baud rate : 30 MHz/(4*(2+1))= 2.5MHz */
-    SSP_Init(SSP0, &SSP_InitStructure);
-
-    SSP_Cmd(SSP0, ENABLE);
 }
 
 void UART0_SetBaudRate( dword baudRate )
@@ -127,118 +92,126 @@ void UART0_WriteFile( const byte *s, dword cnt )
 	while( UART_GetFlagStatus( UART0, UART_FLAG_Busy ) == SET );
 }
 
-void SD_Init()
+void SimpleFill(dword data4)
 {
-    if( ( disk_status(0) & STA_NODISK ) == 0 && ( disk_status(0) & STA_NOINIT ) != 0 )
-    {
-        disk_initialize( 0 );
-
-        if( ( disk_status(0) & STA_NOINIT ) != 0 )
-        {
-            __TRACE( "SD card init error :(\n" );
-        }
-        else
-        {
-            __TRACE( "SD card init OK..\n" );
-
-            static FATFS fatfs;
-            f_mount( 0, &fatfs );
-        }
-    }
-}
-
-void UpdateFirmware()
-{
-    char str[10];
-    if( ( disk_status(0) & STA_NOINIT ) != 0 ) return;
-
-    FIL newFirmware;
-    if( f_open( &newFirmware, "speccy2010.bin", FA_READ ) != FR_OK )
-    {
-        __TRACE( "Cannot open speccy2010.bin...\n" );
-        return;
-    }
-    __TRACE( "Started comparing.\n" );
     int pos;
-    byte data;
-    UINT res;
     byte *flashData = (byte*) MAIN_PROG_START;
-    byte olddata;
-
-    for( pos = 0; pos < newFirmware.fsize; pos++ )
-    {
-        f_read( &newFirmware, &data, 1, &res );
-        olddata = *flashData++;
-        if( data != olddata )
-        {
-            __TRACE("Found difference - at pos =");
-            __TRACE(itoa(pos, str));
-            __TRACE(", value on card = ");
-            __TRACE(itoa(data, str));
-            __TRACE(", value in flash = ");
-            __TRACE(itoa(olddata, str));
-            __TRACE("\n");
-
-             break;
-        }
-    }
-
-    if( pos >= newFirmware.fsize )
-    {
-        __TRACE( "Skipping firmware upgrade.\n" );
-        return;
-    }
-
-    __TRACE( "Firmware upgrade started.\n" );
-
-    FLASH_DeInit();
-
-    f_lseek( &newFirmware, 0 );
-
-    dword data4;
     flashData = (byte*) MAIN_PROG_START;
 
-    for( pos = 0; pos < newFirmware.fsize; pos += 4 )
+    for( pos = MAIN_PROG_START; pos < 0x2003FFFF; pos += 4 )
     {
         if( ( (dword) flashData & 0xfff ) == 0 ) __TRACE( "." );
-
-        if( (dword) flashData == 0x20008000 )
-        {
-            FLASH_EraseSector(FLASH_BANK0_SECTOR4);
-            FLASH_WaitForLastOperation();
-        }
-        else if( (dword) flashData == 0x20010000 )
-        {
-            FLASH_EraseSector(FLASH_BANK0_SECTOR5);
-            FLASH_WaitForLastOperation();
-        }
-        else if( (dword) flashData == 0x20020000 )
-        {
-            FLASH_EraseSector(FLASH_BANK0_SECTOR6);
-            FLASH_WaitForLastOperation();
-        }
-
-        else if( (dword) flashData == 0x20030000 )
-        {
-            FLASH_EraseSector(FLASH_BANK0_SECTOR7);
-            FLASH_WaitForLastOperation();
-        }
-
-        f_read( &newFirmware, &data4, 4, &res );
-
-        FLASH_WriteWord( (dword) flashData - 0x20000000, data4 );
+//        if ((dword) flashData == 0x20012340 ){
+//            FLASH_WriteWord( (dword) flashData - 0x20000000, 0x12345678 );
+//        } else {
+            FLASH_WriteWord( (dword) flashData - 0x20000000, data4 );
+//        }
         FLASH_WaitForLastOperation();
 
         flashData += 4;
     }
-
-    __TRACE( "Firmware upgrade finished.\n" );
+    __TRACE("\n");
 }
-void JumpToMainProg()
+
+void SipleVerify(char data)
 {
-	void (*MainProg)() = (void*) MAIN_PROG_START;
-	__TRACE( "Jumping to MainProg.\n" );
-	MainProg();
+    char str[10];
+
+    int pos;
+    byte *flashData = (byte*) MAIN_PROG_START;
+    flashData = (byte*) MAIN_PROG_START;
+    byte olddata;
+
+    for( pos = MAIN_PROG_START; pos < 0x2003FFFF; pos++ )
+    {
+        olddata = *flashData++;
+        if (olddata != data){
+            __TRACE("Found error - at pos =");
+            __TRACE(itoa(pos, str, 16));
+            __TRACE(", value in flash = ");
+            __TRACE(itoa(olddata, str, 16));
+            __TRACE(", should be = ");
+            __TRACE(itoa(data, str, 16));
+            __TRACE("\n");
+        }
+    }
+}
+
+void ScrambleFill()
+{
+    int pos;
+    unsigned int data;
+    byte *flashData = (byte*) MAIN_PROG_START;
+    flashData = (byte*) MAIN_PROG_START;
+
+    for( pos = MAIN_PROG_START; pos < 0x2003FFFF; pos += 4 )
+    {
+        if( ( (dword) flashData & 0xfff ) == 0 ) __TRACE( "." );
+//        if ((dword) flashData == 0x20012340 ){
+//            FLASH_WriteWord( (dword) flashData - 0x20000000, 0x12345678 );
+//        } else {
+            data = pos % 0x100;
+            data = (data << 8) + data;
+            data = (data << 16) + data;
+            FLASH_WriteWord( (dword) flashData - 0x20000000, data );
+//        }
+        FLASH_WaitForLastOperation();
+
+        flashData += 4;
+    }
+    __TRACE("\n");
+}
+
+void ScrambleVerify()
+{
+    char str[10];
+
+    int pos;
+    unsigned int data;
+    byte *flashData = (byte*) MAIN_PROG_START;
+    flashData = (byte*) MAIN_PROG_START;
+    unsigned int olddata;
+
+    for( pos = MAIN_PROG_START; pos < 0x2003FFFF; pos += 4 )
+    {
+
+        data = pos % 0x100;
+        data = (data << 8) + data;
+        data = (data << 16) + data;
+
+        olddata = *flashData++;
+        olddata = (olddata << 8) + *flashData++;
+        olddata = (olddata << 8) + *flashData++;
+        olddata = (olddata << 8) + *flashData++;
+
+        if (olddata != data){
+            __TRACE("Found error - at pos =");
+            __TRACE(itoa(pos, str, 16));
+            __TRACE(", value in flash = ");
+            __TRACE(itoa(olddata, str, 16));
+            __TRACE(", should be = ");
+            __TRACE(itoa(data, str, 16));
+            __TRACE("\n");
+        }
+    }
+}
+
+void eraseFlash(){
+    __TRACE("Erasing\n");
+    FLASH_EraseSector(FLASH_BANK0_SECTOR1);
+    FLASH_WaitForLastOperation();
+    FLASH_EraseSector(FLASH_BANK0_SECTOR2);
+    FLASH_WaitForLastOperation();
+    FLASH_EraseSector(FLASH_BANK0_SECTOR3);
+    FLASH_WaitForLastOperation();
+    FLASH_EraseSector(FLASH_BANK0_SECTOR4);
+    FLASH_WaitForLastOperation();
+    FLASH_EraseSector(FLASH_BANK0_SECTOR5);
+    FLASH_WaitForLastOperation();
+    FLASH_EraseSector(FLASH_BANK0_SECTOR6);
+    FLASH_WaitForLastOperation();
+    FLASH_EraseSector(FLASH_BANK0_SECTOR7);
+    FLASH_WaitForLastOperation();
 }
 
 int main()
@@ -247,14 +220,30 @@ int main()
 	pllStatusOK = MRCC_Config();
 
     UART0_Init( GPIO0, GPIO_Pin_11, GPIO0, GPIO_Pin_10 );
-    __TRACE( "Speccy2010 boot ver 1.2(extra log for Ricia)!\n" );
+    __TRACE( "Internal flash test with simple scramble\n" );
 
-    SPI_Config();
-    SD_Init();
+    eraseFlash();
+    __TRACE( "Writing 0x55\n" );
+    SimpleFill(0x55555555);
+    __TRACE( "Reading\n" );
+    SipleVerify(0x55);
 
-    UpdateFirmware();
-	JumpToMainProg();
+    eraseFlash();
+    __TRACE( "Writing 0xAA\n" );
+    SimpleFill(0xAAAAAAAA);
+    __TRACE( "Reading\n" );
+    SipleVerify(0xAA);
 
+    eraseFlash();
+    __TRACE( "Writing scramble\n" );
+    ScrambleFill();
+    __TRACE( "Reading scramble\n" );
+    ScrambleVerify();
+
+
+    __TRACE( "The End\n" );
+
+    while (true);
 	return 0;
 }
 
@@ -278,7 +267,7 @@ void WDT_Kick()
 {
 }
 
-char* itoa(int num, char* str)
+char* itoa(int num, char* str, int base)
 {
     int i = 0;
     bool isNegative = false;
@@ -290,7 +279,7 @@ char* itoa(int num, char* str)
         return str;
     }
 
-    if (num < 0)
+    if (num < 0 && base == 10)
     {
         isNegative = true;
         num = -num;
@@ -298,9 +287,9 @@ char* itoa(int num, char* str)
 
     while (num != 0)
     {
-        int rem = num % 10;
+        int rem = num % base;
         str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
-        num = num/10;
+        num = num/base;
     }
 
     if (isNegative)
